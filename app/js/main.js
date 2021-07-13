e9de89695bd07f4f9fed3c1e7db86fd59c85c67f
sasholk <iccc97@gmail.com>
2021-07-13 13:12:57 +0300

    created page 'blog'

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,12 @@
 $(function () {
-    
+
     $('.header__btn').on('click', function () {
         $('.rightside-menu').removeClass('rightside-menu--close');
     });
     $('.rightside-menu__close').on('click', function () {
         $('.rightside-menu').addClass('rightside-menu--close');
     });
-    
+
     $('.top__slider').slick({
         dots: true,
         arrows: false,
@@ -19,6 +19,11 @@ $(function () {
         arrows: false,
         slidesToShow: 10,
         slidesToScroll: 10
+    });
+    
+    $('.article-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="./images/arrow-slide-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="./images/arrow-slide-right.svg" alt=""></button>'
     });
 
     var mixer = mixitup('.gallery__inner', {
