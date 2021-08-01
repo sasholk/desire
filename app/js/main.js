44b34da60a11e83059dca03ac62ac9b990dcc8f4
sasholk <iccc97@gmail.com>
2021-08-01 13:09:30 +0300

    finished to adaptive and builded 'dist'

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,10 @@ $(function () {
         $('.menu').toggleClass('menu--open');
     });
 
+    if($(window).width() < 651){
+        $('.works-path__item--measuring').appendTo($('.works-path__items-box'));
+    };
+
     $('.top__slider').slick({
         dots: true,
         arrows: false,
@@ -22,7 +26,51 @@ $(function () {
         dots: true,
         arrows: false,
         slidesToShow: 10,
-        slidesToScroll: 10
+        slidesToScroll: 10,
+        responsive: [
+            {
+              breakpoint: 1700,
+              settings: {
+                slidesToShow: 8,
+                slidesToScroll: 8,
+              }
+            },
+            {
+              breakpoint: 1510,
+              settings: {
+                slidesToShow: 6,
+                slidesToScroll: 6,
+              }
+            },
+            {
+              breakpoint: 1201,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+              }
+            },
+            {
+              breakpoint: 841,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },
+            {
+              breakpoint: 551,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+              }
+            },
+            {
+              breakpoint: 376,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }
+        ]
     });
     
     $('.article-slider__box').slick({
